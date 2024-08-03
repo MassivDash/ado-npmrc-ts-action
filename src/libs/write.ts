d3142341dd1edfebf002d6a1e7a51a7259504c03
SpaceGhost <lukasz.celitan@gmail.com>
2024-08-03 21:46:14 +0200

    build: adjust lefthook

--- src/libs/write.ts
+++ src/libs/write.ts
@@ -1,14 +1,14 @@
 import fs from 'fs'
 import { Args, encodePassword } from '../libs'
 
-function generateUrl(args: Args): string {
+function generateUrl (args: Args): string {
   if (typeof args.AZURE_PROJECT === 'string' && args.AZURE_PROJECT !== '') {
     return `pkgs.dev.azure.com/${args.AZURE_ORGANIZATION}/${args.AZURE_PROJECT}/_packaging/${args.AZURE_REGISTRY_NAME}/npm`
   }
   return `pkgs.dev.azure.com/${args.AZURE_ORGANIZATION}/_packaging/${args.AZURE_REGISTRY_NAME}/npm`
 }
 
-function generateRegistry(args: Args): string {
+function generateRegistry (args: Args): string {
   let scope = ''
   if (
     typeof args.AZURE_REGISTRY_SCOPE === 'string' &&
@@ -21,7 +21,7 @@ always-auth=true
 `
 }
 
-function generateCredentials(args: Args): string {
+function generateCredentials (args: Args): string {
   const url = generateUrl(args)
   const password = encodePassword(args)
   return `; begin auth token
@@ -34,11 +34,11 @@ function generateCredentials(args: Args): string {
 ; end auth token`
 }
 
-export function generateWriteContent(args: Args): string {
+export function generateWriteContent (args: Args): string {
   return generateRegistry(args) + generateCredentials(args)
 }
 
-export function writeFile(content: string): void {
+export function writeFile (content: string): void {
   // We need to write the file to the current workspace
 
   let path = '.npmrc'
