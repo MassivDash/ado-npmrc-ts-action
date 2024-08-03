ac910d7755cc8bff0beea606821fe2af05d16226
SpaceGhost <lukasz.celitan@gmail.com>
2024-08-03 16:08:49 +0200

    ci(gh): add jest treshhold

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.export = {
+  preset: 'ts-jest',
+  verbose: true,
+  clearMocks: true,
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  coverageReporters: ['json-summary', 'text', 'lcov', 'cobertura'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  moduleNameMapper: {
+    '@libs$': '<rootDir>/src/libs/index.ts',
+    '@libs/(.*)$': '<rootDir>/src/libs/$1'
+  },
+  coverageThreshold: {
+    global: {
+      lines: 80
+    }
+  }
+}
